16c056dc7967cb92cdb1ec53c5b3b4dce770eea8
Alan Dann <alan.dann@shotgunsoftware.com>
2013-02-11 09:59:18 +0000

    #20223 Initial app code for multi publish

---
+++ app.py
@@ -0,0 +1,65 @@
+"""
+Copyright (c) 2013 Shotgun Software, Inc
+----------------------------------------------------
+
+Multi Publish
+
+"""
+
+import os
+import tank
+from tank import TankError
+
+class MultiPublish(tank.platform.Application):
+
+    def init_app(self):
+        """
+        Called as the application is being initialized
+        """
+        
+        tk_multi_publish = self.import_module("tk_multi_publish")
+        
+        
+        """
+        # park this module with Nuke so that the snapshot history UI
+        # pane code can find it later on.
+        nuke._tk_nuke_publish = tk_nuke_publish
+
+        # validate template_work and template_publish have the same extension
+        _, work_ext = os.path.splitext(self.get_template("template_work").definition)
+        _, pub_ext = os.path.splitext(self.get_template("template_publish").definition)
+
+        if work_ext != pub_ext:
+            # disable app
+            self.log_error("'template_work' and 'template_publish' have different file extensions.")
+            return
+
+        # create handlers for our various commands
+        self.write_node_handler = tk_nuke_publish.TankWriteNodeHandler(self)
+        # immediately attach it to the nuke API so that the gizmos can reach it
+        nuke._tank_write_node_handler = self.write_node_handler
+        self.snapshot_handler = tk_nuke_publish.TankSnapshotHandler(self,
+                                                                    self.write_node_handler)
+        self.publish_handler = tk_nuke_publish.TankPublishHandler(self,
+                                                                  self.snapshot_handler,
+                                                                  self.write_node_handler)
+        """
+        
+        def do_publish():
+            pass
+        
+        # register commands:
+        self.engine.register_command("Multi Publish...", do_publish)# (AD) - temp name!
+
+        """
+         # custom panes
+        self.engine.register_command("Tank Snapshot History",
+                                     tk_nuke_publish.snapshot_history.create_new_panel,
+                                     {"type": "custom_pane",
+                                      "panel_id": tk_nuke_publish.snapshot_history.PANEL_UNIQUE_ID})
+
+        self.__add_write_nodes()
+        """
+        
+    def destroy_app(self):
+        self.log_debug("Destroying tk-multi-publish")
---
+++ python/tk_multi_publish/publish_dialog.py
@@ -0,0 +1,112 @@
+"""
+Copyright (c) 2012 Shotgun Software, Inc
+----------------------------------------------------
+"""
+"""
+import tank
+import os
+import sys
+import time
+import threading
+"""
+
+from PySide import QtCore, QtGui
+
+"""
+from .ui.dialog import Ui_Dialog
+from .ui.progress import Ui_Progress
+"""
+
+class PublishDialog(QtGui.QDialog):
+    
+    def __init__(self, app):
+        QtGui.QDialog.__init__(self, parent = QtGui.QApplication.activeWindow())#(AD) - this should be derived from TankDialog
+        self._app = app
+        """
+        # set up the UI
+        self.ui = Ui_Dialog() 
+        self.ui.setupUi(self)
+        
+        # set up the browsers
+        self.ui.left_browser.set_app(self._app)
+        self.ui.left_browser.set_label("Step 1. Choose items to publish")
+        self.ui.left_browser.enable_multi_select(True)
+        self.ui.left_browser.enable_search(False)
+                
+        self.ui.right_browser.set_app(self._app)
+        
+        entity_type = self._app.context.entity.get("type", "")
+        entity_name = self._app.context.entity.get("name", "")
+        
+        self.ui.right_browser.set_label("Step 2. Select a Task (%s %s)" % (entity_type, entity_name))
+        self.ui.right_browser.enable_search(False)
+
+        # refresh when the checkbox is clicked
+        self.ui.hide_tasks.toggled.connect( self._load_tasks )
+        
+        # publish
+        self.ui.publish.clicked.connect( self.accept )
+        
+        # load data from shotgun
+        self._load_scene_contents()    
+        self._load_tasks()    
+        """
+        
+    """
+    ########################################################################################
+    # make sure we trap when the dialog is closed so that we can shut down 
+    # our threads. Nuke does not do proper cleanup on exit.
+    
+    def _cleanup(self):
+        self.ui.left_browser.destroy()
+        self.ui.right_browser.destroy()
+        
+    def closeEvent(self, event):
+        self._cleanup()
+        # okay to close!
+        event.accept()
+        
+    def accept(self):
+        self._cleanup()
+        QtGui.QDialog.accept(self)
+        
+    def reject(self):
+        self._cleanup()
+        QtGui.QDialog.reject(self)
+        
+    def done(self, status):
+        self._cleanup()
+        QtGui.QDialog.done(self, status)
+        
+    ########################################################################################
+    # basic business logic        
+        
+    def _load_scene_contents(self): 
+        self.ui.left_browser.clear()
+        self.ui.left_browser.load({})
+        
+    def _load_tasks(self): 
+        self.ui.right_browser.clear()
+        d = {}
+        d["own_tasks_only"] = self.ui.hide_tasks.isChecked()        
+        self.ui.right_browser.load(d)
+    
+    def get_description(self):
+        return self.ui.comments.toPlainText()
+        
+    def get_task(self):
+        task_item = self.ui.right_browser.get_selected_item()
+        if task_item:
+            return task_item.sg_data
+        else:
+            return None
+    
+    def get_selected_nodes(self):
+        # the mandatory nuke script node returns item.node None so check for that
+        tank_write_nodes = []
+        for x in self.ui.left_browser.get_selected_items():
+            if x.node:
+                tank_write_nodes.append(x.node)
+        return tank_write_nodes
+    """
+        
