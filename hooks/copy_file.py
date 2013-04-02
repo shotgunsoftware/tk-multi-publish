a8b9a34a3c1d9f766d9c4b9ec5e0735817a3f2b4
Alan Dann <alan.dann@shotgunsoftware.com>
2013-04-02 19:45:13 +0100

    ...and the actual hook!

---
+++ hooks/copy_file.py
@@ -0,0 +1,26 @@
+"""
+Copyright (c) 2013 Shotgun Software, Inc
+----------------------------------------------------
+
+"""
+
+import tank
+from tank import Hook
+import shutil
+import os
+
+class CopyFile(Hook):
+    """
+    Hook called when a file needs to be copied
+    """
+    
+    def execute(self, source_path, target_path, **kwargs):
+        
+        # create the folder if it doesn't exist
+        dirname = os.path.dirname(target_path)
+        if not os.path.isdir(dirname):            
+            old_umask = os.umask(0)
+            os.makedirs(dirname, 0777)
+            os.umask(old_umask)            
+
+        shutil.copy(source_path, target_path)
