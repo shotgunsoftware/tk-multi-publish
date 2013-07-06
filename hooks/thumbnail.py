04ae47fd5a637f3e851b77068e0bd4f3606de5b6
manne ohrstrom <manne@shotgunsoftware.com>
2013-07-07 00:53:13 +0100

    Added thumbnail extraction for Hiero.

--- hooks/thumbnail.py
+++ hooks/thumbnail.py
@@ -3,6 +3,7 @@ Copyright (c) 2013 Shotgun Software, Inc
 ----------------------------------------------------
 
 """
+import os
 import tank
 from tank import Hook
 
@@ -21,8 +22,55 @@ class ThumbnailHook(Hook):
                         will be enabled in the UI.
         """
         
-        # Note: the engine name can be accessed from the hook's parent:
-        # engine_name = self.parent.engine.name
+
+        # get the engine name from the parent object (app/engine/etc.)
+        engine_name = self.parent.engine.name
         
-        # default implementation does nothing
+        # depending on engine:
+        if engine_name == "tk-hiero":
+            return self._extract_hiero_thumbnail()
+
+        # default implementation does nothing        
+        return None
+    
+    
+    def _extract_hiero_thumbnail(self):
+        """
+        Extracts the 
+        
+        """
+        import hiero.core
+        from PySide import QtCore
+        import tempfile
+        import uuid
+        
+        # get the menu selection from hiero engine
+        selection = self.parent.engine.get_menu_selection()
+
+        if len(selection) != 1:
+            raise TankError("Please select a single Project!")
+        
+        if not isinstance(selection[0] , hiero.core.Bin):
+            raise TankError("Please select a Hiero Project!")
+            
+        project = selection[0].project()
+        if project is None:
+            # apparently bins can be without projects (child bins I think)
+            raise TankError("Please select a Hiero Project!")
+        
+        # find first sequence with a poster frame
+        for s in project.sequences():            
+            if s.posterFrame():
+                try:
+                    # this sequence has got a poster frame!
+                    thumb_qimage = s.thumbnail(s.posterFrame())
+                    # scale it down to 600px wide
+                    thumb_qimage_scaled = thumb_qimage.scaledToWidth(600, QtCore.Qt.SmoothTransformation)
+                    # save it to tmp location
+                    png_thumb = os.path.join(tempfile.gettempdir(), "sgtk_thumb_%s.png" % uuid.uuid4().hex)
+                    thumb_qimage_scaled.save(png_thumb)
+                    return png_thumb
+                except:
+                    return None
+        
         return None
