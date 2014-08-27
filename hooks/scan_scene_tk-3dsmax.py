26a5af2772714654d50c2934669d43a3b28b3c2d
Mathieu Rivest <mathieu.rivest@autodesk.com>
2014-08-27 10:42:52 -0400

    CR Fixes

--- hooks/scan_scene_tk-3dsmax.py
+++ hooks/scan_scene_tk-3dsmax.py
@@ -9,61 +9,58 @@
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
 import os
+from Py3dsMax import mxs
 
-import MaxPlus
-
-import tank
-from tank import Hook
-from tank import TankError
-
+import sgtk
+from sgtk import Hook
+from sgtk import TankError
 
 class ScanSceneHook(Hook):
     """
     Hook to scan scene for items to publish
     """
-
+    
     def execute(self, **kwargs):
         """
         Main hook entry point
-        :returns:       A list of any items that were found to be published.
-                        Each item in the list should be a dictionary containing
+        :returns:       A list of any items that were found to be published.  
+                        Each item in the list should be a dictionary containing 
                         the following keys:
                         {
                             type:   String
                                     This should match a scene_item_type defined in
-                                    one of the outputs in the configuration and is
-                                    used to determine the outputs that should be
+                                    one of the outputs in the configuration and is 
+                                    used to determine the outputs that should be 
                                     published for the item
-
+                                    
                             name:   String
                                     Name to use for the item in the UI
-
+                            
                             description:    String
                                             Description of the item to use in the UI
-
+                                            
                             selected:       Bool
-                                            Initial selected state of item in the UI.
+                                            Initial selected state of item in the UI.  
                                             Items are selected by default.
-
+                                            
                             required:       Bool
                                             Required state of item in the UI.  If True then
                                             item will not be deselectable.  Items are not
                                             required by default.
-
+                                            
                             other_params:   Dictionary
                                             Optional dictionary that will be passed to the
                                             pre-publish and publish hooks
                         }
-        """
-
+        """   
+                
         items = []
-
+        
         # get the main scene:
-        filename = MaxPlus.FileManager.GetFileNameAndPath().data()
-        if not filename:
+        if not mxs.maxFileName:
             raise TankError("Please Save your file before Publishing")
-
-        # create the primary item - 'type' should match the 'primary_scene_item_type':
-        items.append({"type": "work_file", "name": filename})
+        
+        # create the primary item - 'type' should match the 'primary_scene_item_type':            
+        items.append({"type": "work_file", "name": mxs.maxFileName})
 
         return items
