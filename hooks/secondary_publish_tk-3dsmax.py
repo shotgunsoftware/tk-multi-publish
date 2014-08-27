c8cb38569b724ada6079a678c119a08be276f51e
Mathieu Rivest <mathieu.rivest@autodesk.com>
2014-08-27 10:46:53 -0400

    CR Fixes

--- hooks/secondary_publish_tk-3dsmax.py
+++ hooks/secondary_publish_tk-3dsmax.py
@@ -9,11 +9,12 @@
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
 import os
+import shutil
+from Py3dsMax import mxs
 
-import tank
-from tank import Hook
-from tank import TankError
-
+import sgtk
+from sgtk import Hook
+from sgtk import TankError
 
 class PublishHook(Hook):
     """
@@ -23,70 +24,70 @@ class PublishHook(Hook):
         """
         Main hook entry point
         :param tasks:                   List of secondary tasks to be published.  Each task is a 
-                        dictionary containing the following keys:
-                        {
-                            item:   Dictionary
-                                    This is the item returned by the scan hook 
-                                    {   
-                                        name:           String
-                                        description:    String
-                                        type:           String
-                                        other_params:   Dictionary
-                                    }
-                                   
-                            output: Dictionary
-                                    This is the output as defined in the configuration - the 
-                                    primary output will always be named 'primary' 
-                                    {
-                                        name:             String
-                                        publish_template: template
-                                        tank_type:        String
-                                    }
-                        }
+                                        dictionary containing the following keys:
+                                        {
+                                            item:   Dictionary
+                                                    This is the item returned by the scan hook 
+                                                    {   
+                                                        name:           String
+                                                        description:    String
+                                                        type:           String
+                                                        other_params:   Dictionary
+                                                    }
+                                                   
+                                            output: Dictionary
+                                                    This is the output as defined in the configuration - the 
+                                                    primary output will always be named 'primary' 
+                                                    {
+                                                        name:             String
+                                                        publish_template: template
+                                                        tank_type:        String
+                                                    }
+                                        }
                         
         :param work_template:           template
-                        This is the template defined in the config that
-                        represents the current work file
+                                        This is the template defined in the config that
+                                        represents the current work file
                
         :param comment:                 String
-                        The comment provided for the publish
+                                        The comment provided for the publish
                         
         :param thumbnail:               Path string
-                        The default thumbnail provided for the publish
+                                        The default thumbnail provided for the publish
                         
         :param sg_task:                 Dictionary (shotgun entity description)
-                        The shotgun task to use for the publish    
+                                        The shotgun task to use for the publish    
                         
         :param primary_publish_path:    Path string
-                        This is the path of the primary published file as returned
-                        by the primary publish hook
+                                        This is the path of the primary published file as returned
+                                        by the primary publish hook
                         
         :param progress_cb:             Function
-                        A progress callback to log progress during pre-publish.  Call:
+                                        A progress callback to log progress during pre-publish.  Call:
+                                        
+                                            progress_cb(percentage, msg)
+                                             
+                                        to report progress to the UI
                         
-                            progress_cb(percentage, msg)
-                             
-                        to report progress to the UI
-        
         :param primary_task:            The primary task that was published by the primary publish hook.  Passed
                                         in here for reference.  This is a dictionary in the same format as the
                                         secondary tasks above.
         
-        :returns:       A list of any tasks that had problems that need to be reported 
-                        in the UI.  Each item in the list should be a dictionary containing 
-                        the following keys:
-                        {
-                            task:   Dictionary
-                                    This is the task that was passed into the hook and
-                                    should not be modified
-                                    {
-                                        item:...
-                                        output:...
-                                    }
-                                    
-                            errors: List
-                                    A list of error messages (strings) to report    
-                        }
+        :returns:                       A list of any tasks that had problems that need to be reported 
+                                        in the UI.  Each item in the list should be a dictionary containing 
+                                        the following keys:
+                                        {
+                                            task:   Dictionary
+                                                    This is the task that was passed into the hook and
+                                                    should not be modified
+                                                    {
+                                                        item:...
+                                                        output:...
+                                                    }
+                                                    
+                                            errors: List
+                                                    A list of error messages (strings) to report    
+                                        }
         """
         results = []
         
@@ -114,3 +115,12 @@ class PublishHook(Hook):
             progress_cb(100)
              
         return results
+
+
+
+
+        
+
+
+
+
