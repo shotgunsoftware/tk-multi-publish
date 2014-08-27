0bc2053d2f9e401854362f41e9db8798a6914fa8
Mathieu Rivest <mathieu.rivest@autodesk.com>
2014-08-27 10:45:53 -0400

    CR Fixes

--- hooks/secondary_pre_publish_tk-3dsmax.py
+++ hooks/secondary_pre_publish_tk-3dsmax.py
@@ -9,11 +9,11 @@
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
 import os
+from Py3dsMax import mxs
 
-import tank
-from tank import Hook
-from tank import TankError
-
+import sgtk
+from sgtk import Hook
+from sgtk import TankError
 
 class PrePublishHook(Hook):
     """
@@ -23,54 +23,54 @@ class PrePublishHook(Hook):
         """
         Main hook entry point
         :param tasks:           List of tasks to be pre-published.  Each task is be a 
-                        dictionary containing the following keys:
-                        {   
-                            item:   Dictionary
-                                    This is the item returned by the scan hook 
-                                    {   
-                                        name:           String
-                                        description:    String
-                                        type:           String
-                                        other_params:   Dictionary
-                                    }
-                                   
-                            output: Dictionary
-                                    This is the output as defined in the configuration - the 
-                                    primary output will always be named 'primary' 
-                                    {
-                                        name:             String
-                                        publish_template: template
-                                        tank_type:        String
-                                    }
-                        }
+                                dictionary containing the following keys:
+                                {   
+                                    item:   Dictionary
+                                            This is the item returned by the scan hook 
+                                            {   
+                                                name:           String
+                                                description:    String
+                                                type:           String
+                                                other_params:   Dictionary
+                                            }
+                                           
+                                    output: Dictionary
+                                            This is the output as defined in the configuration - the 
+                                            primary output will always be named 'primary' 
+                                            {
+                                                name:             String
+                                                publish_template: template
+                                                tank_type:        String
+                                            }
+                                }
                         
         :param work_template:   template
-                        This is the template defined in the config that
-                        represents the current work file
+                                This is the template defined in the config that
+                                represents the current work file
                
         :param progress_cb:     Function
-                        A progress callback to log progress during pre-publish.  Call:
+                                A progress callback to log progress during pre-publish.  Call:
+                                
+                                    progress_cb(percentage, msg)
+                                     
+                                to report progress to the UI
                         
-                            progress_cb(percentage, msg)
-                             
-                        to report progress to the UI
-                        
-        :returns:       A list of any tasks that were found which have problems that
-                        need to be reported in the UI.  Each item in the list should
-                        be a dictionary containing the following keys:
-                        {
-                            task:   Dictionary
-                                    This is the task that was passed into the hook and
-                                    should not be modified
-                                    {
-                                        item:...
-                                        output:...
-                                    }
-                                    
-                            errors: List
-                                    A list of error messages (strings) to report    
-                        }
-        """       
+        :returns:               A list of any tasks that were found which have problems that
+                                need to be reported in the UI.  Each item in the list should
+                                be a dictionary containing the following keys:
+                                {
+                                    task:   Dictionary
+                                            This is the task that was passed into the hook and
+                                            should not be modified
+                                            {
+                                                item:...
+                                                output:...
+                                            }
+                                            
+                                    errors: List
+                                            A list of error messages (strings) to report    
+                                }
+        """      
         results = []
         
         # validate tasks:
@@ -97,3 +97,6 @@ class PrePublishHook(Hook):
             progress_cb(100)
             
         return results
+
+    
+    
