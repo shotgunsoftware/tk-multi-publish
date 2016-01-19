b24330662e7c0d645a877b2ba918fb17ac286724
Josh Tomlinson <josh.tomlinson@shotgunsoftware.com>
2016-01-19 09:45:07 -0500

    Adds support for publishing tk alembic/mantra outputs

--- hooks/scan_scene_tk-houdini.py
+++ hooks/scan_scene_tk-houdini.py
@@ -32,22 +32,17 @@ class ScanSceneHook(Hook):
                                     one of the outputs in the configuration and is
                                     used to determine the outputs that should be
                                     published for the item
-
                             name:   String
                                     Name to use for the item in the UI
-
                             description:    String
                                             Description of the item to use in the UI
-
                             selected:       Bool
                                             Initial selected state of item in the UI.
                                             Items are selected by default.
-
                             required:       Bool
                                             Required state of item in the UI.  If True then
                                             item will not be deselectable.  Items are not
                                             required by default.
-
                             other_params:   Dictionary
                                             Optional dictionary that will be passed to the
                                             pre-publish and publish hooks
@@ -65,4 +60,115 @@ class ScanSceneHook(Hook):
 
         # create the primary item - this will match the primary output 'scene_item_type':
         items.append({"type": "work_file", "name": name})
+
+        # look for alembic caches to publish
+        items.extend(self._get_exported_alembic_items())
+
+        # look for rendered images to publish
+        items.extend(self._get_rendered_image_items())
+
         return items
+
+    def _get_exported_alembic_items(self):
+        """Scan the file for tk alembic nodes with already exported caches."""
+
+        app = self.parent
+        
+        # see if the alembicnode app is installed
+        alembic_app = app.engine.apps.get("tk-houdini-alembicnode", None)
+        if not alembic_app:
+            app.log_info(
+                "Will not attempt to scan for alembic caches."
+                "The 'tk-houdini-alembicnode' app is not installed."
+            )
+            return []
+
+        # get all the tk alembic nodes in the scene
+        tk_alembic_nodes = hou.nodeType(hou.ropNodeTypeCategory(),
+            "sgtk_alembic").instances()
+
+        alembic_items = []
+
+        # for each tk alembic node, see if the output file has been 
+        # created on disk. The node will have already evaluated the 
+        # all th fields, so current version should be correct.
+        for tk_alembic_node in tk_alembic_nodes:
+            out_path_parm = tk_alembic_node.parm("filename")
+            out_path = out_path_parm.menuLabels()[out_path_parm.eval()]
+
+            if os.path.exists(out_path):
+                alembic_items.append({
+                    "type": "alembic_cache",
+                    "name": tk_alembic_node.name(),
+                    "other_params": {'path': out_path},
+                })
+
+        return alembic_items
+
+    def _get_rendered_image_items(self):
+        """Scan the file for tk mantra nodes with already rendered images."""
+
+        app = self.parent
+
+        # see if the mantranode app is installed
+        mantra_app = app.engine.apps.get("tk-houdini-mantranode", None)
+        if not mantra_app:
+            app.log_info(
+                "Will not attempt to scan for rendered images."
+                "The 'tk-houdini-mantranode' app is not installed."
+            )
+            return []
+
+        # find all the tk mantra nodes
+        tk_mantra_nodes = hou.nodeType(hou.ropNodeTypeCategory(),
+            "sgtk_mantra").instances()
+
+        # get the current version from the work file
+        work_template = mantra_app.get_template("work_file_template")
+        scene_name = str(hou.hipFile.name())
+        scene_path = os.path.abspath(scene_name)
+        fields = work_template.get_fields(scene_path)
+        cur_version = fields["version"]
+
+        # get the output_profiles for the app
+        output_profiles = {}
+        for output_profile in mantra_app.get_setting("output_profiles"):
+            name = output_profile["name"]
+            output_profiles[name] = output_profile
+
+        render_items = []
+
+        # for each mantra node, see which output profile is selected.
+        # get the template for the selected profile and see if there are 
+        # any images on disk matching the pattern. if so, add them to the
+        # list of rendered items to be returned.
+        for tk_mantra_node in tk_mantra_nodes:
+            output_profile_parm = tk_mantra_node.parm("sgtk_output_profile")
+            output_profile_name = \
+                output_profile_parm.menuLabels()[output_profile_parm.eval()]
+            output_profile = output_profiles[output_profile_name]
+            output_template = mantra_app.get_template_by_name(
+                output_profile["output_render_template"])
+
+            paths = mantra_app.engine.tank.abstract_paths_from_template(
+                    output_template, {"SEQ": "FORMAT: %d", "version": cur_version})
+
+            if not paths:
+                continue
+
+            if len(paths) == 1:
+                if paths:
+                    render_items.append({
+                        "type": "rendered_image",
+                        "name": tk_mantra_node.name(),
+                        "other_params": {'path': paths[0]},
+                    })
+            else:
+                app.log_warning(
+                    "Found multiple potential rendered image paths for "
+                    "mantra node '%s'. Skipping these paths:\n  '%s'" % 
+                    (tk_mantra_node.name(), "\n  ".join(paths))
+                )
+    
+        return render_items
+
--- hooks/secondary_pre_publish_tk-houdini.py
+++ hooks/secondary_pre_publish_tk-houdini.py
@@ -50,7 +50,6 @@ class PrePublishHook(Hook):
                                     progress_cb(percentage, msg)
                                      
                                 to report progress to the UI
-
         :param user_data:       A dictionary containing any data shared by other hooks run prior to
                                 this hook. Additional data may be added to this dictionary that will
                                 then be accessible from user_data in any hooks run after this one.
@@ -82,12 +81,14 @@ class PrePublishHook(Hook):
             # report progress:
             progress_cb(0, "Validating", task)
 
-            # pre-publish item here, e.g.
-            #if output["name"] == "foo":
-            #    ...
-            #else:
-            # don't know how to publish this output types!
-            errors.append("Don't know how to publish this item!")
+            # pre-publish alembic_cache output
+            if output["name"] == "alembic_cache":
+                errors.extend(self.__validate_item_for_alembic_cache_publish(item))
+            elif output["name"] == "rendered_image":
+                errors.extend(self.__validate_item_for_rendered_image_publish(item))
+            else:
+                # don't know how to publish this output types!
+                errors.append("Don't know how to publish this item!")
 
             # if there is anything to report then add to result
             if len(errors) > 0:
@@ -97,3 +98,34 @@ class PrePublishHook(Hook):
             progress_cb(100)
 
         return results
+
+    def __validate_item_for_alembic_cache_publish(self, item):
+        """
+        Validate that the item is valid to be exported to an alembic cache.
+        
+        :param item:    The item to validate
+        :returns:       A list of any errors found during validation that should be reported
+                        to the artist
+        """
+    
+        # validate the exported alembic cache and add any errors found here
+        errors = []
+        
+        # finally return any errors
+        return errors    
+    
+    def __validate_item_for_rendered_image_publish(self, item):
+        """
+        Validate that the item is valid to be exported as rendered images.
+        
+        :param item:    The item to validate
+        :returns:       A list of any errors found during validation that should be reported
+                        to the artist
+        """
+    
+        # validate the exported rendered images and add any errors found here
+        errors = []
+        
+        # finally return any errors
+        return errors    
+
