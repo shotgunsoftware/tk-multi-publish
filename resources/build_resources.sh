45098d54cf7f180b4c74cb5d6d2cd0e906a08e41
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2016-05-31 23:40:04 -0400

    revert changes to the build resources script

--- resources/build_resources.sh
+++ resources/build_resources.sh
@@ -13,9 +13,6 @@
 # The path to output all built .py files to: 
 UI_PYTHON_PATH=../python/tk_multi_publish/ui
 
-# The path to where the PySide binaries are installed
-PYTHON_BASE="/Applications/Shotgun.app/Contents/Resources/Python"
-
 
 # Helper functions to build UI files
 function build_qt {
@@ -29,11 +26,11 @@ function build_qt {
 }
 
 function build_ui {
-    build_qt "${PYTHON_BASE}/bin/python ${PYTHON_BASE}/bin/pyside-uic --from-imports" "$1.ui" "$1"
+    build_qt "pyside-uic --from-imports" "$1.ui" "$1"
 }  
 
 function build_res {
-    build_qt "${PYTHON_BASE}/bin/pyside-rcc" "$1.qrc" "$1_rc"
+    build_qt "pyside-rcc" "$1.qrc" "$1_rc"
 }
 
 
